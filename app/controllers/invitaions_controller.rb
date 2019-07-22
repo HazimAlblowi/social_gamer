fff3a111a5a4fa2422546ed2ed329a12ab07c9fb
Hazim Alblowi <hazim.alblowi@gmail.com>
2019-07-23 01:13:19 +0300

    Adding footer and changing page style

--- app/controllers/invitaions_controller.rb
+++ app/controllers/invitaions_controller.rb
@@ -1,2 +1,34 @@
 class InvitaionsController < ApplicationController
+    before_action :is_admin, only: [:new, :create, :edit, :update, :destroy]
+
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def set_invitaion
+        @invitaion = Invitaion.find(params[:id])
+    end
+    
+    def dog_params
+        params.require(:invitaion).permit(:game, :platform, :time)
+    end
 end
